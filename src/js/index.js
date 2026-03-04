48645ac9ed7140b9653a86d8d2a7e536745b6ef1
GerrysatriaH <gerrysatria10@gmail.com>
2026-03-04 09:27:01 +0700

    minor updates

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  AOS.init({
+    duration: 800,
+    once: false,
+    offset: 100,
+  });
+});
+
+const themeToggle = document.getElementById('theme-toggle');
+      const htmlElement = document.documentElement;
+      const savedTheme = localStorage.getItem('theme');
+      
+      if (savedTheme) {
+        htmlElement.setAttribute('data-theme', savedTheme);
+        themeToggle.checked = savedTheme === 'dark';
+      } else {
+        htmlElement.setAttribute('data-theme', 'light');
+      }
+
+      themeToggle.addEventListener('change', (e) => {
+        if (e.target.checked) {
+          htmlElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark'); 
+        } else {
+          htmlElement.setAttribute('data-theme', 'light'); 
+          localStorage.setItem('theme', 'light'); 
+        }
+      });
